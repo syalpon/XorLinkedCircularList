ccaaacce55105ab83ed9607f694dacb2794e9628
syalpon <onoue0107@gmail.com>
2022-01-09 19:32:38 +0900

    header 修正(cpp対応)

--- xlcl.h
+++ xlcl.h
@@ -1,11 +1,19 @@
 #ifndef _XLCL_H_
 #define _XLCL_H_
 
-__declspec(dllimport) void *__cdecl xlcl_start(void);
-__declspec(dllimport) void *__cdecl xlcl_add_node(void *vp,int additional_data);
-__declspec(dllimport) int   __cdecl xlcl_pop(void *vp);
-__declspec(dllimport) void  __cdecl xlcl_all_show(void *vp);
-__declspec(dllimport) void *__cdecl xlcl_end(void *vp);
+    #ifdef __cplusplus
+    extern "C" {
+    #endif
+
+    __declspec(dllimport) void *__cdecl xlcl_start(void);
+    __declspec(dllimport) void *__cdecl xlcl_add_node(void *vp,int additional_data);
+    __declspec(dllimport) int   __cdecl xlcl_pop(void *vp);
+    __declspec(dllimport) void  __cdecl xlcl_all_show(void *vp);
+    __declspec(dllimport) void *__cdecl xlcl_end(void *vp);
+    
+    #ifdef __cplusplus
+    }
+    #endif
 
 #endif
 
